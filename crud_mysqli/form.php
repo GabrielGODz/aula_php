b22956a7c4a967a157fc448f3f23e56150716808
Gabriel <zgabrielvitorino@gmail.com>
2024-03-27 22:14:53 -0300

    Aula_27_03

--- crud_mysqli/form.php
+++ crud_mysqli/form.php
@@ -1,3 +1,42 @@
+<?php
+
+$pk_cliente = "";
+$nome = "";
+$cpf = "";
+$whatsapp = "";
+$email = "";
+
+// VERIFICA SE EXISTE UMA VARIÁVEL NA URL CHAMADA "REF"
+if (isset($_GET['ref'])) {
+    $pk_cliente = base64_decode(trim($_GET['ref']));
+
+    include('../conexao_mysqli.php');
+    $sql = "
+    SELECT *
+    FROM clientes
+    WHERE pk_cliente = '$pk_cliente'
+    ";
+
+    $query = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($query) > 0) {
+        $row = mysqli_fetch_object($query);
+        $nome = $row->NOME;
+        $cpf = $row->CPF;
+        $whatsapp = $row->WHATSAPP;
+        $email = $row->EMAIL;
+    } else {
+        echo    '
+        <script>
+            alert   ("Registro não encontrado.");
+            window  .   location="./"
+        </script>
+        ';
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -22,30 +61,30 @@
                             <div class="row mb-3">
                                 <div class="col-md-1">
                                     <label for="pk_cliente" class="form-label">ID</label>
-                                    <input readonly type="text" id="pk_cliente" name="pk_cliente" class="form-control">
+                                    <input value="<?php echo $pk_cliente?>" readonly type="text" id="pk_cliente" name="pk_cliente" class="form-control">
                                 </div>
                                 <div class="col">
                                     <label for="nome" class="form-label">Nome</label>
-                                    <input type="text" id="nome" name="nome" class="form-control" required>
+                                    <input value="<?php echo $nome?>" type="text" id="nome" name="nome" class="form-control" required>
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <div class="col">
                                     <label for="cpf" class="form-label">CPF</label>
-                                    <input type="text" id="cpf" name="cpf" class="form-control" data-mask="000.000.000-00" minlength="14" required>
+                                    <input value="<?php echo $cpf?>" type="text" id="cpf" name="cpf" class="form-control" data-mask="000.000.000-00" minlength="14" required>
                                 </div>
                                 <div class="col">
                                     <label for="whatsapp" class="form-label">WhatsApp</label>
-                                    <input type="text" id="whatsapp" name="whatsapp" class="form-control">
+                                    <input value="<?php echo $whatsapp?>" type="text" id="whatsapp" name="whatsapp" class="form-control">
                                 </div>
                                 <div class="col">
                                     <label for="email" class="form-label">E-mail</label>
-                                    <input type="email" id="email" name="email" class="form-control" required>
+                                    <input value="<?php echo $email?>" type="email" id="email" name="email" class="form-control" required>
                                 </div>
                             </div>
                         </div>
                         <div class="card-footer text-end">
-                            <button type="button" class="btn btn-outline-danger"><i class="bi bi-arrow-left"> Voltar</i></button>
+                            <a class="btn btn-outline-danger" href="index.php"><i class="bi bi-arrow-left"> Voltar</i></a>
                             <button type="submit" class="btn btn-outline-primary"><i class="bi bi-floppy"> Salvar</i></button>
                         </div>
                     </div>
@@ -54,7 +93,7 @@
         </div>
     </div>
 
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -65,7 +104,7 @@
             onKeyPress: function(whatsapp, e, field, options) {
                 var masks = ['(00) 0000-000#', '(00) 00000-0000'];
                 var mask = (whatsapp.length > 14) ? masks[1] : masks[0];
-                $('#whatsapp').mask(mask,options);
+                $('#whatsapp').mask(mask, options);
             }
 
         };
---
+++ crud_mysqli/remover.php
@@ -0,0 +1,44 @@
+<?php
+
+if (isset($_GET['ref'])) {
+    $pk_cliente = base64_decode(trim($_GET['ref']));
+
+    include('../conexao_mysqli.php');
+
+    $sql = "
+    DELETE FROM clientes
+    WHERE pk_cliente = '$pk_cliente' ";
+
+    $query = mysqli_query($conn, $sql);
+
+    if ($query) {
+        $msg = "Registro removido com sucesso!";
+    } else {
+        $msg = "Error: " . mysqli_error($conn);
+    }
+
+    echo "<script> alert('$msg')
+        window.location='./'; 
+        </script>";
+    exit;
+}
+
+header('Location: ./');
+exit;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
